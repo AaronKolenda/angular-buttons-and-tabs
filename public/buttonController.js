d5e95fc7daff496ec35b0072f71e0434415babff
AaronKolenda <kolenda.a@gmail.com>
2015-03-17 15:17:07 -0400

    all functionality

--- public/buttonController.js
+++ public/buttonController.js
@@ -3,6 +3,24 @@ angular.module("buttonController", [])
 
 
   // Your code goes here
+  $scope.clicked = false;
+  $scope.text = "Enabled";
+
+  $scope.disableButton = function() {
+
+ 	$scope.clicked = true;
+	$scope.text = "Loading...";
+
+  	$timeout(function(){
+      
+      $scope.clicked = false;
+      $scope.text = "Enabled";
+
+    }, 4000);
+  	
+  }
+
+
 
 
 }])
--- public/tabController.js
+++ public/tabController.js
@@ -1,8 +1,44 @@
 angular.module("tabController", [])
 .controller("TabController", ["$scope", function($scope){
 
+	$scope.something = "ok";
+	console.log($scope);
 
-  // Your code goes here
+
+	// Your code goes here
+	$scope.redValue = true;
+	$scope.greenValue = false;
+	$scope.blueValue = false;
+
+	$scope.redClick = function() {
+		console.log("red clicked");
+		if ($scope.redValue === false) {
+			$scope.redValue = true;
+			$scope.greenValue = false;
+			$scope.blueValue = false;
+		};
+
+	};
+
+	$scope.greenClick = function() {
+		console.log("green clicked");
+		if ($scope.greenValue === false) {
+			$scope.greenValue = true;
+			$scope.redValue = false;
+			$scope.blueValue = false;
+		};
+	};
+
+	$scope.blueClick = function() {
+		console.log("blue clicked");
+		if ($scope.blueValue === false) {
+			$scope.blueValue = true;
+			$scope.greenValue = false;
+			$scope.redValue = false;
+		};
+	};
+
+
   
   
 }])
